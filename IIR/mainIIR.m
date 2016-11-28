68a230aa94d51f2591e63803577f8dc91a35ce71
Dupont <samuel.dupont.etu@univ-lemans.fr>
2016-11-28 17:16:38 +0100

    matlab program

--- IIR/mainIIR.m
+++ IIR/mainIIR.m
@@ -8,6 +8,9 @@
 %                   formulation in order to produce the filter coefficients.
 %                   It departs from the analog coefficient, then using the
 %                   bilinear transform it produces the digital numbers;
+% 
+%                   The value of the filter can be modified in the first
+%                   part of the program.
 %                 
 %
 %  Reference : 
@@ -55,22 +58,21 @@
         xlim([p.w(1)/(2*pi) p.w(end)/(2*pi)])
         grid on
 %% Change to z domain using bilinear transform with warping method 1    
+    c.W0= 2/c.TS * tan( pi * c.FC / c.FS);% warped cut off freq
+    f.pk =-exp(1i.* (2.*v.k+c.ORDER-1)*pi / (2*c.ORDER));% normalised pole
+    c.B=2./(c.TS*c.W0);% n front of bilinear transform (with warped coef)
+    
+    f.pkz2 = (c.B-f.pk) ./ (c.B+f.pk);% discrete pole
+    f.gz2 = (1./prod(c.B+f.pk));% gain
+    f.zk = ones(c.ORDER,1);% zeros
 
-    f.pk =-exp(1i.* (2.*v.k+c.ORDER-1)*pi / (2*c.ORDER));
-    c.W0= c.WC;
-    c.B=c.W0./(c.WC*tan(c.W0*c.TS/2));
-
-    f.pkz2 = (c.B-f.pk) ./ (c.B+f.pk);
-    f.gz2 = (1./prod(c.B+f.pk));
-    f.zk = -ones(c.ORDER,1);
-      
-    [b,a] = zp2tf(f.zk,f.pkz2,f.gz2);
+    [b,a] = zp2tf(f.zk,f.pkz2,f.gz2);%transform zeros poles form into transfert function (b,a)
     
     % plot
     figure(2)
     
     subplot(211)
-    [p.h2,p.w2]=freqz(b,a);
+    [p.h2,p.w2]=freqz(b,a);%obtain bode from b,a coef
         plot(p.w2/pi,db(abs(p.h2)))
         grid on
         p.dyn=-min(db(abs(p.h2)))+ round(max(db(abs(p.h2))));
@@ -87,78 +89,46 @@
         ylabel('Angle [rad]');
         xlim([p.w2(1)/(2*pi) p.w2(end)/(2*pi)])
         grid on   
-%% Change to z domain using bilinear transform with prewarping method 2
-    c.W0= 2/c.TS * tan( pi * c.FC / c.FS);
-    f.pk =-exp(1i.* (2.*v.k+c.ORDER-1)*pi / (2*c.ORDER));
-    c.B=2./(c.TS*c.W0);
-
-
-    f.pkz2 = (c.B-f.pk) ./ (c.B+f.pk);
-    f.gz2 = real(1./prod(c.B+f.pk));
-    f.zk = -ones(c.ORDER,1);
-     
-    % plot
-    figure(3)
-    
-    [b,a] = zp2tf(f.zk,f.pkz2,f.gz2);
-    [p.h2,p.w2]=freqz(b,a);
-    
-    subplot(211)
-        plot(p.w2/pi,db(abs(p.h2)))
-        grid on
-        p.dyn=-min(db(abs(p.h2)))+ round(max(db(abs(p.h2))));
-        axis([p.w2(1)/pi p.w2(end)/pi min(db(abs(p.h2))) round(max(db(abs(p.h2))))+p.dyn*0.05]);
-        xlabel('Frequency [Hz]');
-        ylabel('Amplitude [dB]');
-        hold on
-        plot([c.FC/c.NY c.FC/c.NY],[min(db(abs(p.h2))) round(max(db(abs(p.h2))))+p.dyn*0.05],'g')
-        text(c.FC/c.NY,-3,'\leftarrow Fc = -3dB')
-   
-    subplot(212)
-        plot(p.w2/(2*pi),unwrap(angle(p.h2)))
-        xlabel('Frequency [Hz]');
-        ylabel('Angle [rad]');
-        xlim([p.w2(1)/(2*pi) p.w2(end)/(2*pi)])
-        grid on           
+        
 %% Difference between warped and normal
-opt=1;
-if opt==1
-    c.FC=[3000  15000];
-    c.WC=2*pi*c.FC;
-    for ii=1:length(c.WC)
-        % warped
-        c.W0= 2/c.TS * tan( c.WC(ii)/2 / c.FS);
-        f.pk =-exp(1i.* (2.*v.k+c.ORDER-1)*pi / (2*c.ORDER));
-        c.B=2./(c.TS*c.W0);
-        f.pkz2 = (c.B-f.pk) ./ (c.B+f.pk);
-        f.gz2 = (1./prod(c.B+f.pk));
+    opt=0;
+    if opt==1
+        c.FC=[3000  15000];
+        c.WC=2*pi*c.FC;
+        for ii=1:length(c.WC)
+            % warped coefficient (like before)
+            c.W0= 2/c.TS * tan( c.WC(ii)/2 / c.FS);
+            f.pk =-exp(1i.* (2.*v.k+c.ORDER-1)*pi / (2*c.ORDER));
+            c.B=2./(c.TS*c.W0);
+            f.pkz2 = (c.B-f.pk) ./ (c.B+f.pk);
+            f.gz2 = (1./prod(c.B+f.pk));
 
-        [b,a] = zp2tf(f.zk,f.pkz2,f.gz2);
-        [p.h2,p.w2]=freqz(b,a);
+            [b,a] = zp2tf(f.zk,f.pkz2,f.gz2);
+            [p.h2,p.w2]=freqz(b,a);
 
-        % normal
-        f.pkz2 = (c.B-f.pk) ./ (c.B+f.pk);
-        f.gz2 = (1./prod(c.B+f.pk));
+            % normal (without warped coefficient)
+            f.pkz2 = (c.B-f.pk) ./ (c.B+f.pk);
+            f.gz2 = (1./prod(c.B+f.pk));
 
-        c.B=2./(c.TS*c.WC(ii));
-        f.pkz3 = (c.B-f.pk) ./ (c.B+f.pk);
-        f.gz3 = (1./prod(c.B+f.pk));
+            c.B=2./(c.TS*c.WC(ii));
+            f.pkz3 = (c.B-f.pk) ./ (c.B+f.pk);
+            f.gz3 = (1./prod(c.B+f.pk));
 
-        [b,a] = zp2tf(f.zk,f.pkz3,f.gz3);
-        [p.h3,p.w3]=freqz(b,a);
+            [b,a] = zp2tf(f.zk,f.pkz3,f.gz3);
+            [p.h3,p.w3]=freqz(b,a);
 
-        %plot
-        figure(4)
+            %plot
+            figure(4)
 
-            plot(p.w2/pi,db(abs(p.h2)),'b')
-            hold on
-            grid on
-            plot(p.w3/pi,db(abs(p.h3)),'r--')
-        xlabel('Frequency [Hz]');
-        ylabel('Amplitude [dB]');
-        text(c.FC(ii)/c.NY,-3,'\leftarrow Fc = -3dB')
-        plot([c.FC(ii)/c.NY c.FC(ii)/c.NY],[min(db(abs(p.h2))) round(max(db(abs(p.h2))))+p.dyn*0.05],'g')
-        axis([p.w2(1)/pi p.w2(end)/pi min(db(abs(p.h2))) round(max(db(abs(p.h2))))+p.dyn*0.05]);
+                plot(p.w2/pi,db(abs(p.h2)),'b')
+                hold on
+                grid on
+                plot(p.w3/pi,db(abs(p.h3)),'r--')
+            xlabel('Frequency [Hz]');
+            ylabel('Amplitude [dB]');
+            text(c.FC(ii)/c.NY,-3,'\leftarrow Fc = -3dB')
+            plot([c.FC(ii)/c.NY c.FC(ii)/c.NY],[min(db(abs(p.h2))) round(max(db(abs(p.h2))))+p.dyn*0.05],'g')
+            axis([p.w2(1)/pi p.w2(end)/pi min(db(abs(p.h2))) round(max(db(abs(p.h2))))+p.dyn*0.05]);
+        end
+           legend('warped', 'unwarped','location','southwest')
     end
-       legend('warped', 'unwarped','location','southwest')
-end
--- IIR/mainIIR_hp_bp_bs.m
+++ IIR/mainIIR_hp_bp_bs.m
@@ -1,10 +1,10 @@
 % 
-%  File    :   mainIIR_Biquad
+%  File    :   mainIIR_hp_bp_bs
 % 
 % Author1: Samuel Dupont
 % Date:    November 2016 
 %   
-%  Description :  Explore IIR filtering departing from analitical
+%  Description :  Explore IIR filtering for high pass departing from analitical
 %                   formulation in order to produce the filter coefficients.
 %                   It departs from the analog coefficient, then using the
 %                   bilinear transform it produces the digital numbers;
@@ -18,8 +18,8 @@
 %                 https://en.wikipedia.org/wiki/Butterworth_filter
 %                 https://en.wikipedia.org/wiki/Bilinear_transform
     clear variables; close all; clc
-%% Define the various constant of the filter
 
+    %% Define the various constant of the filter
     c.FS = 44100;
     c.TS = 1/c.FS;
     c.NY = c.FS/2;
@@ -34,60 +34,61 @@
     K=2/(c.TS*c.WC);
     K2=K^2;
     
-%% Create the prototype continuous filter
+    %% Create the prototype continuous filter
     Coef=zeros(round(c.ORDER/2),6);
     fin=1:round(c.ORDER/2)-1;
-
     last=round(c.ORDER/2);
 
-%     Coef(ii,1) = 0;
-%     Coef(ii,2) = 0
-    Coef(fin,3) = 1;
+    Coef(fin,1) = 1;
+    Coef(fin,2) = 0;
+    Coef(fin,3) = 0;
     Coef(fin,4) = 1;
     Coef(fin,5) = -2*cos(pi*(2*fin+c.ORDER-1)/2/c.ORDER);
     Coef(fin,6) = 1;
 
     if mod(c.ORDER,2)==0
-        Coef(end,3) = 1;
+        Coef(end,1) = 1;
+        Coef(end,2) = 0;
+        Coef(end,3) = 0;
         Coef(end,4) = 1;
         Coef(end,5) = -2*cos(pi*(2*last+c.ORDER-1)/2/c.ORDER);
         Coef(end,6) = 1;
     else
-        Coef(end,3) = 1;
-        Coef(end,4) = 0;
+        Coef(end,1) = 1;
+        Coef(end,2) = 0;
+        Coef(end,3) = 0;
+        Coef(end,4) = 1;
         Coef(end,5) = 1;
-        Coef(end,6) = 1;
+        Coef(end,6) = 0;
     end
 
 %% Calculate digital filter coefficients
     Coefd=zeros(last,6);
     
     Coefd(fin,1) = 1;
-    Coefd(fin,2) = 2;
+    Coefd(fin,2) = -2*K2./ ( K2 );
     Coefd(fin,3) = 1;
     Coefd(fin,4) = 1;
     Coefd(fin,5) = ( 2 - 2*K2 ) ./ ( K2 + Coef(fin,5)*K + 1 );
     Coefd(fin,6) = ( K2 -Coef(fin,5)*K + 1 ) ./ ( K2 + Coef(fin,5)*K + 1 );
-    Gz = prod(1./(K2 + Coef(fin,5)*K + 1));
-
+    
+    Gz = prod(K2./( K2 + Coef(fin,5)*K + 1));
+    
     if mod(c.ORDER,2)==1
-        Coefd(end,1) = Gz / ( Coef(last,5)*K +1 );
-        Coefd(end,2) = Gz / ( Coef(last,5)*K +1 );
-        Coefd(end,4) = 1;
-        Coefd(end,5) = ( -Coef(last,5)*K +1 ) / ( Coef(last,5)*K +1 );
+        Coefd(last,1) = Gz*K / ( Coef(last,5) +K );
+        Coefd(last,2) = -Gz*K / ( Coef(last,5) +K );
+        Coefd(last,4) = 1;
+        Coefd(last,5) = ( +Coef(last,5) -K ) / ( Coef(last,5) +K );
     else
-        Coefd(end,1) = Gz./ ( K2 + Coef(last,5)*K + 1 );
-        Coefd(end,2) = 2*Gz./ ( K2 + Coef(last,5)*K + 1 );
-        Coefd(end,3) = Gz./ ( K2 + Coef(last,5)*K + 1 );
-        Coefd(end,4) = 1;
-        Coefd(end,5) = ( 2 - 2*K2 ) ./ ( K2 + Coef(last,5)*K + 1 );
-        Coefd(end,6) = ( K2 -Coef(last,5)*K + 1 ) ./ ( K2 + Coef(last,5)*K + 1 );
+        Coefd(last,1) = Gz*K2./ ( K2 + Coef(last,5)*K + 1 );
+        Coefd(last,2) = -Gz*2*K2./ ( K2 + Coef(last,5)*K + 1 );
+        Coefd(last,3) = Gz*K2./ ( K2 + Coef(last,5)*K + 1 );
+        Coefd(last,4) = 1;
+        Coefd(last,5) = ( 2 - 2*K2 ) ./ ( K2 + Coef(last,5)*K + 1 );
+        Coefd(last,6) = ( K2 -Coef(last,5)*K + 1 ) ./ ( K2 + Coef(last,5)*K + 1 );
     end
 
-%% Change from low pass to high pass
-%   Coefd(:,2)=-Coefd(:,2);Coefd(last,1:3)=Coefd(last,1:3)*K^c.ORDER;%freqz(Coefd,10000);%highpass
-
-%% plot frequency response and verification
+%% verif
     freqz(Coefd(1:3),Coefd(4:6),10000);
     hold on
-   [b,a]=butter(c.ORDER,c.FC/c.NY,'high');sos=tf2sos(b,a);freqz(sos,10000)
+    [b,a]=butter(c.ORDER,c.FC/c.NY,'high');sos=tf2sos(b,a);freqz(b,a)
